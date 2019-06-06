b48b15e8d75979a25a498d32782cb8111df9354b
Maayan Tzoubari <maayan.tzoubari@gmail.com>
2019-06-06 17:23:42 +0200

    added randomization shuffle

--- js/memory.js
+++ js/memory.js
@@ -1,48 +1,50 @@
 var superHeroes = [
     {
         name: "flash",
-        img: "url('../images/flash.jpg')",
+        img: "url('./images/flash.jpg')",
     },
     {
         name: "wonderwoman",
-        img: "url('../images/wonderwoman.jpg')",
+        img: "url('./images/wonderwoman.jpg')",
     },
     {
         name: "marvel",
-        img: "url('../images/marvel.jpg')",
+        img: "url('./images/marvel.jpg')",
     },
     {
         name: "spiderman",
-        img: "url('../images/spiderman.jpg')",
+        img: "url('./images/spiderman.jpg')",
     },
     {
         name: "ironman",
-        img: "url('../images/ironman.jpg')",
+        img: "url('./images/ironman.jpg')",
     },
     {
         name: "hulk",
-        img: "url('../images/hulk.jpg')",
+        img: "url('./images/hulk.jpg')",
     },
 ]
-$.each(superHeroes, function(index,value){
+
+//double the objects to provide a match for each:
+
+var cardMatches = superHeroes.concat(superHeroes);
+
+//shuffle the cards:
+
+cardMatches.sort (function(a,b) {
+    return 0.5 - Math.random();
+});
+
+//create new card for each object and set image:
+
+$.each(cardMatches, function(index,value){
     console.log(value);
     var newCard=$("<div />");
     newCard.addClass("col-xs-6 col-md-3 card anon");
-    newCard.data("name", superHeroes[index]["name"]);
-    newCard.css("background-img", superHeroes[index]["img"]);
+    newCard.data("name", cardMatches[index]["name"]);
+    newCard.css("background-image", cardMatches[index]["img"]);
+    newCard.css("background-size", "cover");
     $(".row").append(newCard);
 })
 
 
-
-//to display content of each card when clicked on:
-
-/*
-cards.each(function () {
-    $(this).on("click", function () {
-        for (var i = 0; i < superHeroes.length; i++) {
-            $(this).toggleClass("" + superHeroes[i].name + "");
-        }
-    })
-});
-*/
